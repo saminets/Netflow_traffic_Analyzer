e766e9c80f9a944e4cfeb8dcda6fa6d1d8490a5a
saminets <samiullah@nets-international.com>
2024-11-29 15:06:59 +0500

    add cors enabled

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Request
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
 from core.database import Base, engine
 from api.routes.analytics_router import analytics_router
 # Create tables in the database
@@ -7,5 +8,24 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update with allowed origins, e.g., ["https://example.com"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+# Custom Logging Middleware
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    # Log the request
+    print(f"Request: {request.method} {request.url}")
+    response = await call_next(request)
+    # Log the response status code
+    print(f"Response status: {response.status_code}")
+    return response
 # Include the analytics routes
 app.include_router(analytics_router, prefix="/analytics", tags=["Analytics"])
